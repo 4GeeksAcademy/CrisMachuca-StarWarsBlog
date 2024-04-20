af32cba796b562c82a509c5191a9f65483c38cdb
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-04-20 01:52:37 +0000

    navbar barra busqueda

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -8,30 +8,51 @@ export const Navbar = () => {
       <span className="navbar-toggler-icon" ></span>
     </button>
     
-   
-    <a className="navbar-brand" href="#">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg" height="50" alt=""/>
-    </a>
     
     
-    <div className="collapse navbar-collapse" id="opciones">   
+    <div className="collapse navbar-collapse d-flex flex-column" id="opciones">   
+	<div className="navsuperior container-fluid d-flex align-items-center justify-content-between">
+		<div className="social d-flex">
+			<a><i className="fa-brands fa-github"></i></a>
+			<a><i className="fa-brands fa-linkedin"></i></a>
+			<a><i className="fa-brands fa-github"></i></a>
+		</div>
+		<div className="navbar-brand" href="#">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg" height="90" alt="" className="logo"/>
+		</div>
+		<div>
+		<form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-warning" type="submit">Search</button>
+      </form>
+	  </div>
+	</div>
       <ul className="navbar-nav">
         <li className="nav-item">
-		<Link to="/" className="link">
-		<i className="fa-solid fa-house"></i> <span className="ms-1 d-none d-sm-inline">Star Wars</span>
-				</Link>
-          
+			<Link to="/" className="nav-link">
+				<i className="fa-solid fa-house"></i> <span className="ms-1 d-none d-sm-inline">Home</span>
+			</Link> 
         </li>
+
         <li className="nav-item">
-          <a className="nav-link" href="#"><i className="fa-solid fa-earth-americas"></i> <span className="ms-1 d-none d-sm-inline">Planets</span></a>
+			<Link to="/" className="nav-link">
+				<i className="fa-solid fa-earth-americas"></i> <span className="ms-1 d-none d-sm-inline">Planets</span>
+			</Link>
         </li>
+
         <li className="nav-item">
-          <a className="nav-link" href="#"><i className="fa-solid fa-shuttle-space"></i> <span className="ms-1 d-none d-sm-inline">Vehicles</span></a>
+			<Link to="/" className="nav-link">
+		  		<i className="fa-solid fa-shuttle-space"></i> <span className="ms-1 d-none d-sm-inline">Vehicles</span>
+		  	</Link>
         </li>
+
         <li className="nav-item">
-          <a className="nav-link" href="#"><i className="fa-solid fa-user-astronaut"></i> <span className="ms-1 d-none d-sm-inline">Characters</span></a>
+			<Link to="/" className="nav-link">
+				<i className="fa-solid fa-user-astronaut"></i> <span className="ms-1 d-none d-sm-inline">Characters</span>
+			</Link>
         </li>            
       </ul>
+	  
     </div>
   </nav>
 
